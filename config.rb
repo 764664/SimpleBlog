86c813f9bbd9a98cd280edd91b70f91dfa9266b4
764664 <764664@gmail.com>
2013-12-06 20:21:13 +0800

    Fix.

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ TITLE = "吕杰's Blog"
 SUBTITLE = "Coder & Software User"
 LINKS = [{:name => "我的Github", :link => "https://github.com/764664/"},
 {:name => "我的记事本", :link => "http://lvjie.me:10002"},
-{:name => "HTTP信息"}, :link => "/ip"]
+{:name => "HTTP信息", :link => "/ip"} ]
 PORT = 10001
 MDDIR = "md/"
 LINKPREFIX = "post"
