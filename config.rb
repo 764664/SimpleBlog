1bf60a9c47154ba0585804bf5e728cb8e80fbe69
764664 <764664@gmail.com>
2013-12-06 17:12:39 +0800

    Add link to my notepad.

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 TITLE = "吕杰's Blog"
 SUBTITLE = "Coder & Software User"
-LINKS = [{:name => "我的Github", :link => "https://github.com/764664/"}]
+LINKS = [{:name => "我的Github", :link => "https://github.com/764664/"},
+{:name => "我的记事本", :link => "http://lvjie.me:10002"}]
 PORT = 10001
 MDDIR = "md/"
 LINKPREFIX = "post"
