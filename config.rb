28202cbe1349824346c6f8d13638784eb5013590
root <root@pluto.lvjie.me>
2013-12-31 11:21:57 +0800

    Update blog.

--- config.rb
+++
@@ -1,8 +0,0 @@
-TITLE = "吕杰's Blog"
-SUBTITLE = "Coder & Software User"
-LINKS = [{:name => "我的Github", :link => "https://github.com/764664/"},
-{:name => "我的记事本", :link => "/notepad"},
-{:name => "HTTP信息", :link => "/httpinfo"} ]
-PORT = 10001
-MDDIR = "md/"
-LINKPREFIX = "post"
