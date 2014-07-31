18a5999435b0b769c090b6f1976ff9ed85e920aa
吕杰 <764664@gmail.com>
2014-07-31 20:19:20 +0800

    Introduce haml

--- app.rb
+++ app.rb
@@ -108,13 +108,13 @@ class MyBlog < Sinatra::Base
   
   get '/' do
     #redirect to 'http://en.lvjie.me' if request.env["HTTP_ACCEPT_LANGUAGE"] != "zh-cn"
-    erb :index, :locals => {:meta => meta, :articles => articles.keys.sort.reverse.first(10).map{ |n| articles[n] }, 
+    haml :index, :locals => {:meta => meta, :articles => articles.keys.sort.reverse.first(10).map{ |n| articles[n] }, 
     :page => 1, :pages => (articles.size-1)/config["postinpage"]+1}
   end
   
   get '/page/:page' do |page|
     page = page.to_i
-    erb :index, :locals => {:meta => meta, :articles => articles.keys.sort.reverse[(page-1)*10..-1].first(10).map{ |n| articles[n] }, 
+    haml :index, :locals => {:meta => meta, :articles => articles.keys.sort.reverse[(page-1)*10..-1].first(10).map{ |n| articles[n] }, 
     :page => page, :pages => (articles.size-1)/config["postinpage"]+1}
   end
   
